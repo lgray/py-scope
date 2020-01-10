36daf8f985a1ef7372e576aca90c2571247c345b
Lindsey Gray <lindsey.gray@gmail.com>
2020-01-09 18:10:33 -0600

    improvements and more shake down

--- py-scope.py
+++ py-scope.py
@@ -4,6 +4,8 @@ from time import sleep
 import binascii
 import yaml
 import numpy as np
+import h5py
+import zmq
 from concurrent.futures import ProcessPoolExecutor
 
 DEFAULT_LONG_READ = 1 << 24
@@ -14,6 +16,9 @@ class scope(object):
         self.sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.verbose = conf['verbose']        
         self.timeout = conf['timeout']
+        if self.timeout == 'None': 
+            self.timeout = None
+        self.daq_timeout = conf['daq_timeout'] if 'daq_timeout' in conf.keys() else conf['timeout']
         self.sck.connect((conf['hostname'], conf['port']))
         self.ro_query = {'horizontal': 'HORizontal:ACQLENGTH?;:WFMOutpre:XINcr?;:WFMOutpre:PT_Off?',
                          'fastframe': 'HORizontal:FASTframe:STATE?;:HORizontal:FASTframe:COUNt?',
@@ -27,11 +32,11 @@ class scope(object):
     def send_cmd(self, cmd, readsize=DEFAULT_LONG_READ):
         if self.verbose: 
             print('sending ->',cmd)        
-        padded = cmd + '\n'    
+        padded = cmd + '\n'
         self.sck.send(padded.encode())
         if 'CURVE' in cmd:
             chunks = []
-            self.sck.settimeout(self.timeout)
+            self.sck.settimeout(self.daq_timeout)
             rcd = b''
             while True:
                 try:
@@ -40,12 +45,15 @@ class scope(object):
                         break
                 except Exception:
                     pass
-            while len(rcd) > 0:
-                chunks.append(rcd)                
+            chunks.append(rcd)
+            while len(b''.join(chunks)) < readsize:                
                 try:
-                    rcd = self.sck.recv(readsize)
+                    rcd = self.sck.recv(readsize)                
                 except Exception:
                     rcd = b''
+                chunks.append(rcd)
+                
+            self.sck.settimeout(self.timeout)            
             return b''.join(chunks)
         elif '?' in cmd:
             return self.sck.recv(4096).decode().strip()
@@ -78,18 +86,21 @@ class scope(object):
                 out['nFrames'] = 0 if not out['fastframe'] else int(ret[1])
         sum_bytes = 0
         for i in range(SCOPE_NCHANNELS):
-            sum_bytes += out['vertical%d' % (i+1)][3]*out['chmask'][i]
+            channel_bytes = (1 if not out['fastframe'] else out['nFrames']) * out['nPt'] * out['vertical%d' % (i+1)][3]
+            str_ch_bytes = str(channel_bytes)
+            header = '#'+hex(len(str_ch_bytes))[-1].upper()+str_ch_bytes
+            sum_bytes += out['chmask'][i]*(channel_bytes + len(header))
+        sum_bytes += 1 # for the '\n' at the end
         out['readout_size'] = out['nPt']
-        out['readout_size_bytes'] = out['nPt'] * sum_bytes
+        out['readout_size_bytes'] = sum_bytes
         if out['fastframe'] and out['nFrames'] > 0:
-            out['readout_size'] = out['nPt'] * out['nFrames']
-            out['readout_size_bytes'] = out['nPt'] * out['nFrames'] * sum_bytes
+            out['readout_size'] = out['nPt'] * out['nFrames']            
         ro_ch = []
         for i, read in enumerate(out['chmask']):
             if read:
                 ro_ch.append(i+1)
         source_cmd = 'DATA:SOURCE ' + ','.join(['CH%d' % i for i in ro_ch])
-        self.send_cmd(source_cmd)
+        self.send_cmd(source_cmd)        
         return out
         
 
@@ -98,6 +109,9 @@ myscope = None
 states = {}
 tasks = None
 print('opening', sys.argv[1])
+if len(sys.argv) > 2:
+    print('saving to', sys.argv[2])
+    out_file = sys.argv[2]
 with open(sys.argv[1]) as f:    
     config = yaml.load_all(f, Loader=yaml.SafeLoader)
     for conf in config:
@@ -118,7 +132,7 @@ def acq(thescope, loop_spec, header_info):
     socket.bind('tcp://0.0.0.0:33373')
     nevents = 0
     # need to calculate expected read size based on header info
-    readsize = DEFAULT_LONG_READ
+    readsize = header_info['readout_size_bytes']
     while True:
         try:
             for step in loop_spec:
@@ -152,7 +166,7 @@ def unpack_buffers(data, header_info):
         assert data[0] == 35
         nbytes_len = int(data[1:2], 16)
         nbytes_payload = int(data[2:2+nbytes_len])
-        out = data[2+nbytes_len:]
+        out = data[2+nbytes_len:]        
         return nbytes_payload, out
     
     bufsize, unpack = consume_header(unpack)
@@ -172,7 +186,7 @@ def writer(out_file,header_info):
     context = zmq.Context()
     rsck = context.socket(zmq.PULL)
     rsck.bind('tcp://0.0.0.0:33374')
-    outf = h5py.File('myfile.hdf5','w')
+    outf = h5py.File(out_file,'w')
     dset = outf.create_dataset("waveform",
                                (header_info['nch'], 0),
                                maxshape=(header_info['nch'], None),
@@ -190,7 +204,7 @@ def writer(out_file,header_info):
             nevents += 1
             dset.resize((header_info['nch'], nevents*header_info['readout_size']))
             dset[:,(nevents-1)*header_info['readout_size']:nevents*header_info['readout_size']] = stacked
-            print('saved %d events' % nevents, end= '\r')
+            print('saved %d events' % (nevents * ( header_info['nFrames'] if header_info['fastframe'] else 1 )) , end= '\r')
         except KeyboardInterrupt:
             break
     outf.close()
@@ -220,9 +234,12 @@ if tasks is not None:
             run_acq_loop(myscope, states[task], out_file)
         else:
             for command in states[task]:
-                out = myscope.send_cmd(command)                
-                if 'CURVE' in command or '?' in command:
-                    print(out)
+                try:
+                    out = myscope.send_cmd(command)                
+                    if 'CURVE' in command or '?' in command:
+                        print(out)
+                except KeyboardInterrupt:
+                    pass
 
 myscope.shutdown()
 
