fix bug in dqm tres calculation

data_plotter.py
@@ -49,8 +49,11 @@ def calculate_voltages_raw(v_in, pedestal_length=350):
     v_preamp_adjusted = np.copy(v_in)
     for i in range(v_in.shape[0]):
         ped = 0.0
-        popt, pcov = curve_fit(const, np.arange(v_in.shape[1]), v_in[i, :pedestal_length], p0=[ped])
-        ped = popt[0]
+        try:
+            popt, pcov = curve_fit(const, np.arange(v_in.shape[1]), v_in[i, :pedestal_length], p0=[ped])
+            ped = popt[0]
+        except RuntimeError:
+            ped = np.mean(v_in[i, :pedestal_length])
         v_preamp_adjusted[i] = v_preamp_adjusted[i] - ped
     return v_preamp_adjusted
 
@@ -72,7 +75,10 @@ def calculate_tcross(v_in, percent_thresh, dt, gain_post=-10, pedestal_length=40
         if idx_max[evt] > 0:
             spline = InterpolatedUnivariateSpline(time, v_preamp_pedsub[evt])        
             start = time[np.argmin(abs(spline(time[:idx_max[evt]])-threshold[evt]))]
-            t0 = optimize.newton(lambda x: spline(x)-threshold[evt], x0=start, maxiter=500)
+            try:
+                t0 = optimize.newton(lambda x: spline(x)-threshold[evt], x0=start, maxiter=500)
+            except RuntimeError:
+                t0 = np.nan
         t0s[evt] = t0
     
     return t0s
@@ -340,7 +346,7 @@ def plot_waveforms(time, v_ch1, v_ch2, v_ch3, v_ch4, pp, xlable="Time(ns)", ylab
 def plotting_job(afile, scope_config, outfile):
     from matplotlib.backends.backend_pdf import PdfPages
     tc = scope_config['transcond']['lowgain']
-    tcucsc = scope_config['transcond']['UCSC']
+    tcucsc = scope_config['transcond']['lowgain']#scope_config['transcond']['UCSC']
     data, attrs = extract_dataset(afile)    
     pp = PdfPages(outfile)
     trigger_t0s = None
@@ -351,7 +357,7 @@ def plotting_job(afile, scope_config, outfile):
     maxv_ch3 = np.max(calculate_voltages(data[2], gain_post=np.sign(scope_config['gains'][2])), axis=-1)
     
     ch1 = (maxv_ch1 > 0.050) & (maxv_ch1 < 0.272)
-    ch2 = (maxv_ch2 > 0.050) & (maxv_ch2 < 0.272)
+    ch2 = (maxv_ch2 > 0.010) & (maxv_ch2 < 0.272)
     ch3 = (maxv_ch3 > 0.030) & (maxv_ch3 < 0.272)
 
     #print(ch1.shape, ch2.shape, ch3.shape)
@@ -387,11 +393,14 @@ def plotting_job(afile, scope_config, outfile):
             else:
                 measure_t0s[ch] = t0s
         
+    fit_plot_range = (-1.500, 1.500)
+    nbins = 250
+
     fig, ax = plt.subplots(1, 1, dpi=400)
     tch21_avg = (t0s_simple[0][0] - t0s_simple[1][0])[mask]
     tch21_mean = np.mean(tch21_avg)
     tch21_sigma = np.std(tch21_avg, ddof=1)
-    bins, edges = np.histogram(tch21_avg, 500, range=(-1.500, 1.500),  density=False)
+    bins, edges = np.histogram(tch21_avg, nbins, range=fit_plot_range,  density=False)
     centers = 0.5*(edges[1:] + edges[:-1])
     try:
         popt, pcov = curve_fit(gaus,centers,bins,p0=[1,tch21_mean,tch21_sigma])
@@ -400,46 +409,51 @@ def plotting_job(afile, scope_config, outfile):
         tch21_sigma = abs(popt[2])
     except:
         pass
-    ax.hist(tch21_avg, 500, range=(-1.500, 1.500), 
+    ax.hist(tch21_avg, nbins, range=fit_plot_range, 
             density=False,
-            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch21_mean,tch21_sigma,tch21_avg.size,500))
+            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch21_mean,tch21_sigma,tch21_avg.size, nbins))
     ax.legend()
     ax.set(xlabel='t_2 - t_1  (ns)', ylabel='Counts',
            title='TOA for CH2 vs CH1')
     pp.savefig(fig)
     plt.close(fig)
 
-    tch21_sigma = np.std(tch21_avg, ddof=1)
+    print('21 from fit', tch21_sigma)
+    #tch21_sigma = norm.fit(tch21_avg)[1]
+    print('21 from refit', tch21_sigma)
 
     fig, ax = plt.subplots(1, 1, dpi=400)
     tch32_avg = (t0s_simple[2][0] - t0s_simple[1][0])[mask]
     tch32_mean = np.mean(tch32_avg)
-    tch32_sigma = np.std(tch32_avg, ddof=1)
-    bins, edges = np.histogram(tch32_avg, 500, range=(-1.500, 1.500), density=False)
+    tch32_sigma = norm.fit(tch32_avg)[1]
+    bins, edges = np.histogram(tch32_avg, nbins, range=fit_plot_range, density=False)
     centers = 0.5*(edges[1:] + edges[:-1])
     try:
         popt, pcov = curve_fit(gaus,centers,bins,p0=[1,tch32_mean,tch32_sigma])
         ax.plot(centers, gaus(centers,popt[0], popt[1], popt[2]))
-        tch2_mean = popt[1]
-        tch2_sigma = abs(popt[2])
+        tch32_mean = popt[1]
+        tch32_sigma = abs(popt[2])
+    
     except:
         pass
-    ax.hist(tch32_avg, 500, range=(-1.500, 1.500), 
+    ax.hist(tch32_avg, nbins, range=fit_plot_range, 
             density=False,
-            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch32_mean,tch32_sigma,tch32_avg.size,500))
+            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch32_mean,tch32_sigma,tch32_avg.size,nbins))
     ax.legend()
     ax.set(xlabel='t_2 - t_3 (ns)', ylabel='Counts',
            title='TOA for CH2 vs CH3')
     pp.savefig(fig)
     plt.close(fig)
 
-    tch32_sigma = np.std(tch32_avg, ddof=1)
+    print('32 from fit', tch32_sigma)
+    #tch32_sigma = norm.fit(tch32_avg)[1]
+    print('32 from refit', tch32_sigma)
 
     fig, ax = plt.subplots(1, 1, dpi=400)
     tch31_avg = (t0s_simple[2][0] - t0s_simple[0][0])[mask]
     tch31_mean = np.mean(tch31_avg)
     tch31_sigma = np.std(tch31_avg, ddof=1)
-    bins, edges = np.histogram(tch31_avg, 500, range=(-1.50, 1.50), density=False)
+    bins, edges = np.histogram(tch31_avg, nbins, range=fit_plot_range, density=False)
     centers = 0.5*(edges[1:] + edges[:-1])
     try:
         popt, pcov = curve_fit(gaus,centers,bins,p0=[1,tch31_mean,tch31_sigma])
@@ -448,22 +462,25 @@ def plotting_job(afile, scope_config, outfile):
         tch31_sigma = abs(popt[2])
     except:
         pass
-    ax.hist(tch31_avg, 500, range=(-1.50, 1.50), 
+    ax.hist(tch31_avg, nbins, range=fit_plot_range, 
             density=False,
-            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch31_mean,tch31_sigma,tch31_avg.size,500))
+            label='mean = %.3g ns\nsigma = %.3g ns\n#event = %d\n#bin = %d'%(tch31_mean,tch31_sigma,tch31_avg.size,nbins))
     ax.legend()
     ax.set(xlabel='t_1 - t_3 (ns)', ylabel='Counts',
            title='TOA for CH1 vs CH3')
     pp.savefig(fig)
     plt.close(fig)
     
-    tch31_sigma = np.std(tch31_avg, ddof=1)
+    print('31 from fit',tch31_sigma)
+    #tch31_sigma = norm.fit(tch31_avg)[1]
+    print('31 from refit', tch31_sigma)
 
     fig, ax = plt.subplots(1, 1, dpi=400)
     tch2_avg = (0.5*(t0s_simple[0][0]+t0s_simple[2][0]) - t0s_simple[1][0])[mask]
     tch2_mean = np.mean(tch2_avg)
     tch2_sigma = np.std(tch2_avg, ddof=1)
-    bins, edges = np.histogram(tch2_avg, 500, range=(-1.500, 1.500),  density=False)
+    print('sigma_231 basic', tch2_sigma)
+    bins, edges = np.histogram(tch2_avg, nbins, range=fit_plot_range,  density=False)
     centers = 0.5*(edges[1:] + edges[:-1])
     try:
         popt, pcov = curve_fit(gaus,centers,bins,p0=[1,tch2_mean,tch2_sigma])
@@ -472,12 +489,19 @@ def plotting_job(afile, scope_config, outfile):
         tch2_sigma = abs(popt[2])
     except:
         pass
-        
+
+    print('sigma_231 after fit', tch2_sigma)
+
+    bayes_info = scipy.stats.bayes_mvs(tch2_avg, alpha=0.68)
+    print('231', np.std(tch2_avg, ddof=1), bayes_info[2][0])
+    print('231\'', scipy.stats.bayes_mvs(tch2_avg[np.abs(tch2_avg - bayes_info[0][0]) < 5*bayes_info[2][0]]))
+    
+
     sigma_sens_2 = np.sqrt(0.5*(tch21_sigma**2 - tch31_sigma**2 + tch32_sigma**2))
 
-    ax.hist(tch2_avg, 500, range=(-1.500, 1.500), 
+    ax.hist(tch2_avg, nbins, range=fit_plot_range, 
             density=False,
-            label='sigma = %.3g ns\nCH2 Jitter = %.3g ns\n#event = %d\n#bin = %d'%(tch2_sigma, sigma_sens_2, tch2_avg.size,500))
+            label='sigma = %.3g ns\nCH2 Jitter = %.3g ns\n#event = %d\n#bin = %d'%(tch2_sigma, sigma_sens_2, tch2_avg.size,nbins))
     ax.legend()
     ax.set(xlabel='t_2 - 0.5*(t_1 + t_3) (ns)', ylabel='Counts',
            title='TOA for CH2 vs average of CH1+CH3')
@@ -496,7 +520,7 @@ def plotting_job(afile, scope_config, outfile):
 
 gain_post = -10.0
 scope_config = {'trigger': 2,
-                'transcond':{'highgain': 15.7e3, 'lowgain': 4.4e3, 'UCSC': 4.7e3},
+                'transcond':{'highgain': 15.7e3, 'lowgain': 4.4e3, 'UCSC': 4.7e2},
                 'gains': [gain_post, gain_post, gain_post, 1.0],
                 'thresholds': [0.5, 0.5, 0.5, 0.5]}
 